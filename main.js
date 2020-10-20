6d9ee23f285f0e97a9d5c879fae897c365b5e876
acarrmo <a.carrmo@hotmail.com>
2020-10-19 19:25:00 -0500

    Corrección en directorio del script.

--- main.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('color').addEventListener('input',getColor)
-
-function getColor()
-{
-  let col = document.getElementById('color').value
-
-  document.getElementById('container').style.background=col
-  document.getElementById('container').innerHTML=col
-}
