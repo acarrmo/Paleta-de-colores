b58281e1c877a0e5a90ea17185a59b1e7e84467d
acarrmo <a.carrmo@hotmail.com>
2020-10-20 17:20:48 -0500

    Corrección de puntos y comas.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-document.getElementById('color').addEventListener('input',getColor)
+document.getElementById('color').addEventListener('input',getColor);
 
 function getColor()
 {
-  let col = document.getElementById('color').value
+  let col = document.getElementById('color').value;
 
-  document.getElementById('container').style.background=col
-  document.getElementById('container').innerHTML=col
+  document.getElementById('container').style.background=col;
+  document.getElementById('container').innerHTML=col;
 }
